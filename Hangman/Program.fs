189c3c610e9e84e619ee66eaba487646f09a6843
Robert Andersson <inputfalken@gmail.com>
2017-12-05 21:06:37 +0100

    Initial commit

---
+++ Hangman/Program.fs
@@ -0,0 +1,20 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open System
+[<EntryPoint>]
+let main argv = 
+    let word = "word"
+    let Guess (keyFn: Unit -> Char) = 
+        let key = keyFn()
+        if String.exists (fun x -> key = x) word then Some(key)
+        else None
+
+    let  CheckHistory (letter: Char Option) = 
+        ()
+
+
+    let guess = Guess (Console.ReadKey >> (fun cki -> cki.KeyChar))
+
+
+    0 // return an integer exit code
