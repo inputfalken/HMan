60f1ff246c4390ec272a677dcbe13141a38e6dd0
Robert Andersson <inputfalken@gmail.com>
2017-12-06 23:24:06 +0100

    Create recursive function to represent the game

--- Hangman/Program.fs
+++ Hangman/Program.fs
@@ -5,16 +5,25 @@ open System
 [<EntryPoint>]
 let main argv = 
     let word = "word"
+
     let Guess (keyFn: Unit -> Char) = 
         let key = keyFn()
-        if String.exists (fun x -> key = x) word then Some(key)
-        else None
+        if word |> String.exists (fun x -> key = x) then Some(key) else None
+    
+    let NewGuess (guesses: List<char>) (guess: char) =
+        if guesses |> List.exists (fun x -> x = guess) then None else Some(guess)
 
-    let  CheckHistory (letter: Char Option) = 
-        ()
+    let rec Game (guesses: List<char>) (attempts: int) =
+        let (>>=) x s = Option.bind s x
 
+        let guess = Guess ((fun () -> Console.ReadKey(true)) >> (fun cki -> cki.KeyChar))
+        let res = guess >>= (fun x -> NewGuess guesses x)
+        let output = match res with
+                     | Some x -> sprintf "Success char: %c" x
+                     | None -> "Fail char"
 
-    let guess = Guess ((fun () -> Console.ReadKey(true)) >> (fun cki -> cki.KeyChar))
+        printfn "%s" output
+        Game guesses attempts + 1
 
-
+    let score = Game [] 0
     0 // return an integer exit code
