bd627c8fd7823077ed0a618884226852a388174e
Robert Andersson <inputfalken@gmail.com>
2017-12-10 21:25:43 +0100

    Add word progess and refactor structure

--- Hangman/Program.fs
+++ Hangman/Program.fs
@@ -1,38 +1,46 @@
 ﻿open System
 [<EntryPoint>]
-let main argv = 
-    let Hangman  (word: string) (inputFn: Unit -> char) (outputFn: string -> Unit) = 
-        let CorrectGuess (key: char) (word: string) = 
-            if word |> String.exists (fun x -> key = x) then Some(key) else None
+let main argv =
+    let CorrectGuess (key: char) (word: string) =
+        if word |> String.exists (fun x -> key = x) then Some(key) else None
 
-        let SaveToHistory (history: List<char>) (item: char) =
-            (history, [item]) ||> List.append 
+    let SaveToHistory (history: List<char>) (item: char) =
+        (history, [item]) ||> List.append
 
-        let GameWon word history =
-            if word |> String.forall (fun x ->  history |> List.contains x)  then Some(word) else None
-        
-        let rec Guess (history: list<char>) = 
-            let res = inputFn()
-            if (res, history) ||> List.contains then 
-                sprintf "Letter %c has allready been guessed." res |> outputFn
+    let GameWon word history =
+        if word |> String.forall (fun x -> history |> List.contains x) then Some(word) else None
+    
+    let WordProgress (word: string) history =
+        word |> Seq.toList |> List.map (fun x -> if (x, history) ||> List.contains then Some(x) else None)
+
+    let Game (word: string) (input: Unit -> char) (output: string -> Unit) = 
+        let OutputWordProgress word history =
+            (word, history) ||> WordProgress |> List.map (fun x -> match x with | Some x -> x | None -> '_') |> List.toArray |> (fun s -> s |> String) |> output
+
+        let rec Guess (history: list<char>) =
+            let res = input()
+            if (res, history) ||> List.contains then
+                sprintf "Letter %c has allready been guessed." res |> output
+                (word, history) ||>  OutputWordProgress
                 Guess history
-            else res 
+            else res
 
-        let rec Game (history: List<char>) (attempts: int) =
+        let rec Turn (history: List<char>) (attempts: int) =
             let letter = history |> Guess
-            let history = (history, letter) ||> SaveToHistory 
+            let history = (history, letter) ||> SaveToHistory
             let correctGuess = (letter, word) ||> CorrectGuess
 
             match correctGuess with
-            | Some letter -> sprintf "Letter '%c' is correct!" letter |> outputFn
-            | None -> sprintf "Letter '%c' is incorrect!" letter |> outputFn
+            | Some letter -> sprintf "Letter '%c' is correct!" letter |> output
+            | None -> sprintf "Letter '%c' is incorrect!" letter |> output
 
+            (word, history) ||> OutputWordProgress
             match correctGuess |> Option.bind (fun _ -> GameWon word history) with
             | Some _ -> attempts
-            | None -> Game history attempts + 1
+            | None -> Turn history attempts + 1
 
-        let attempts = Game [] 1
-        sprintf "Game finished, attempts required: '%i'" attempts |> outputFn
+        let attempts = Turn [] 1
+        sprintf "Game finished, attempts required: '%i'" attempts |> output
     
-    Hangman "word" ((fun () -> Console.ReadKey(true)) >> fun cki -> cki.KeyChar) Console.WriteLine
+    Game "woooord" ((fun () -> Console.ReadKey(true)) >> fun cki -> cki.KeyChar) Console.WriteLine
     0
