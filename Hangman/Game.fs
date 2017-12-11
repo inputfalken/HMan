f28811e6e058f93f0b42814d35add889ba98546f
Robert Andersson <inputfalken@gmail.com>
2017-12-11 23:05:50 +0100

    Put Gamme in seperate file

---
+++ Hangman/Game.fs
@@ -0,0 +1,47 @@
+﻿module Game
+open System
+
+let CorrectGuess (key: char) (word: string) =
+    if word |> String.exists (fun x -> key = x) then Some(key) else None
+
+let SaveToHistory (history: List<char>) (item: char) =
+    (history, [item]) ||> List.append
+
+let GameWon word history =
+    if word |> String.forall (fun x -> history |> List.contains x) then Some(word) else None
+
+let GameLost maxAttempts attempts =
+    if attempts >= maxAttempts then Some(attempts) else None
+
+let WordProgress (word: string) history =
+    word |> Seq.toList |> Seq.map (fun x -> if (x, history) ||> Seq.contains then Some(x) else None)
+
+let Game (word: string) (input: unit -> char) (output: string -> unit) (clear:  unit -> unit) = 
+    let OutputWordProgress word history =
+         (word, history) ||> WordProgress |> Seq.map (fun x -> match x with | Some x -> x | None -> '_') |> Seq.toArray |> String |> output
+
+    let rec Guess (history: List<char>) =
+        let res = input()
+        clear()
+        if (res, history) ||> List.contains then
+            res |> sprintf "Letter '%c' has allready been guessed." |> output
+            (word, history) ||>  OutputWordProgress
+            Guess history
+        else res
+
+    let rec Turn (history: List<char>) (attempts: int) =
+        let letter = history |> Guess
+        let history = (history, letter) ||> SaveToHistory
+        let correctGuess = (letter, word) ||> CorrectGuess
+
+        match correctGuess with
+        | Some _ -> letter |> sprintf "Letter '%c' is correct!" |> output
+        | None -> letter |> sprintf "Letter '%c' is incorrect!" |> output
+
+        (word, history) ||> OutputWordProgress
+        match correctGuess |> Option.bind (fun _ -> GameWon word history) with
+        | Some _ -> attempts
+        | None -> Turn history attempts + 1
+
+    let attempts = Turn [] 1
+    attempts |> sprintf "Game finished, attempts required: '%i'" |> output
--- Hangman/Program.fs
+++ Hangman/Program.fs
@@ -1,50 +1,8 @@
-﻿open System
+﻿module Program
+open Game
+open System
+
 [<EntryPoint>]
 let main argv =
-    let CorrectGuess (key: char) (word: string) =
-        if word |> String.exists (fun x -> key = x) then Some(key) else None
-
-    let SaveToHistory (history: List<char>) (item: char) =
-        (history, [item]) ||> List.append
-
-    let GameWon word history =
-        if word |> String.forall (fun x -> history |> List.contains x) then Some(word) else None
-
-    let GameLost maxAttempts attempts =
-        if attempts >= maxAttempts then Some(attempts) else None
-    
-    let WordProgress (word: string) history =
-        word |> Seq.toList |> List.map (fun x -> if (x, history) ||> List.contains then Some(x) else None)
-
-    let Game (word: String) (input: Unit -> Char) (output: String -> Unit) (clear:  Unit -> Unit) = 
-        let OutputWordProgress word history =
-            (word, history) ||> WordProgress |> List.map (fun x -> match x with | Some x -> x | None -> '_') |> List.toArray |> (fun s -> s |> String) |> output
-
-        let rec Guess (history: List<char>) =
-            let res = input()
-            clear()
-            if (res, history) ||> List.contains then
-                res |> sprintf "Letter '%c' has allready been guessed." |> output
-                (word, history) ||>  OutputWordProgress
-                Guess history
-            else res
-
-        let rec Turn (history: List<char>) (attempts: int) =
-            let letter = history |> Guess
-            let history = (history, letter) ||> SaveToHistory
-            let correctGuess = (letter, word) ||> CorrectGuess
-
-            match correctGuess with
-            | Some _ -> letter |> sprintf "Letter '%c' is correct!" |> output
-            | None -> letter |> sprintf "Letter '%c' is incorrect!" |> output
-
-            (word, history) ||> OutputWordProgress
-            match correctGuess |> Option.bind (fun _ -> GameWon word history) with
-            | Some _ -> attempts
-            | None -> Turn history attempts + 1
-
-        let attempts = Turn [] 1
-        attempts |> sprintf "Game finished, attempts required: '%i'" |> output
-    
     Game "woooord" ((fun () -> Console.ReadKey(true)) >> fun cki -> cki.KeyChar) Console.WriteLine Console.Clear
     0
